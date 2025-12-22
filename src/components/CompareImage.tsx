6cebc293e0b4b2b29c22e70698e2504e6bb54435
omsrivastava512 <79087911+omsrivastava512@users.noreply.github.com>
2025-12-22 23:51:19 +0530

    feat(ui): [Day 7] Refactor image expansion logic: Move expanded state to parent component - Lifted state management for image expansion to CompareImage for centralized control. - Replaced local isExpanded state in ExpandableImage with a single expandedImage state in parent so that only one image can expand at a time. - Expanding one image now automatically hides others

--- src/components/CompareImage.tsx
+++ src/components/CompareImage.tsx
@@ -13,13 +13,40 @@ import nature from "../assets/nature.png"
  */
 
 const CompareImage = () => {
+    const [expandedImage, setExpandedImage] = useState<number | null>(null)
+
+    const toggleExpand = (i: number) => {
+        setExpandedImage(e => e === i ? null : i)
+    }
+
     return (<div className="relative max-w-screen max-h-screen size-screen">
         <div className="flex justify-center gap-1.5 p-2">
-            <ExpandableImage layoutOn />
-            <ExpandableImage />
+            <ControlledExpandableImage key={2} isHidden={expandedImage !== null} isExpanded={expandedImage == 2} toggleExpand={() => toggleExpand(2)} />
+            <ControlledExpandableImage key={1} layoutOn isHidden={expandedImage !== null} isExpanded={expandedImage == 1} toggleExpand={() => toggleExpand(1)} />
         </div>
         <h1>Click to Expand</h1>
     </div>
+    )
+}
+
+type ControlledExpandableImageProps = {
+    isExpanded: boolean,
+    isHidden: boolean,
+    layoutOn?: boolean,
+    toggleExpand(): void
+}
+
+const ControlledExpandableImage = ({ isExpanded, isHidden, layoutOn = false, toggleExpand }: ControlledExpandableImageProps) => {
+
+    return (
+        <div className={`${isHidden && !isExpanded && 'opacity-0'} border-4 w-50 rounded-lg ${isExpanded && 'border-transparent'} transition-all duration-300`}>
+            <h3 className={`${isExpanded && 'opacity-0'} transition-all duration-300`} >Layout {layoutOn ? "On" : "Off"}</h3>
+            <motion.img
+                src={nature} alt="Expandable" layout={layoutOn} title={layoutOn ? "layout on" : "layout off"}
+                className={`${isExpanded ? 'fixed top-0 left-0  w-dvw z-10' : 'relative w-50 z-0 '} rounded`}
+                onClick={toggleExpand}
+            />
+        </div>
     )
 }
 
